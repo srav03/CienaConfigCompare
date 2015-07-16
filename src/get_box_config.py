a7d451379391b493ddcafc8346f3b1b96fe4a7dc
Sravanthi <sravanthi.nalamalapu@gmail.com>
2015-07-16 15:08:35 -0400

    Telnet working. Needs more

--- src/get_box_config.py
+++ src/get_box_config.py
@@ -1,24 +1,29 @@
 __author__ = 'snalam200'
 import telnetlib
 import sys
-import getpass
-
 
 class CurrentConfig(object):
     def __init__(self):
         pass
 
-    def login_secure(self):
-        tn = telnetlib.Telnet()
-        tn.read_until("login: ")
-        password = getpass.getpass()
-        if password:
-            tn.read_until("Password: ")
-            tn.write(password + "\n")
 
-        tn.write("ls \n")
-        tn.write("exit \n")
-        print tn.read_all()
+def login_secure():
+    tn = telnetlib.Telnet()
+    print "Enter IP address or FQDN:"
+    ip_fqdn = raw_input()
+    tn.open(ip_fqdn)
+    print tn.read_until("username: ")
+    tn.write(raw_input("Enter username") + '\r\n')
+    #tn.read_until("password: ")
+    print tn.read_until("password: ")
+    #tn.write(password + "\n")
+    tn.write(raw_input("Enter pass") + '\r\n')
+    #print tn.read_all()
+    tn.write("terminal length 0" + "\r\n")
+    print tn.read_all()
+    tn.write("config show\f\n")
+    print tn.read_all()
+    tn.close()
 
 
 
